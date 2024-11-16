f0d2081a4cf9dde0094e6c4f2b9fd6ddf05f52ee
Rich Smith <rich.smith@sheridancollege.ca>
2024-11-16 11:56:01 -0500

    Testing last

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @GetMapping("/")
     public String hello()
     {
-        return "HelloWorld!!!";
+        return "HelloWorld!!!  Yay!!! Too Cool!!!!";
     }
 
 }

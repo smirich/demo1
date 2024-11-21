b84e7fd885ed6711d615ef5284b6e7ddc0994a70
Rich Smith <rich.smith@sheridancollege.ca>
2024-11-21 14:21:02 -0500

    new change

---
+++ src/main/java/com/example/demo/SpringController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringController {
+
+    @GetMapping("/joke")
+    public String getJoke()
+    {
+        return "HelloWorld!!!  Yay!!! Too Cool!!!!  Tada!";
+    }
+
+}
